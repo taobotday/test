62c631a0f17d7a7f3b9b91d5b8125b5dc6adc7cb
Minh Khiêm <87948587+taobotday@users.noreply.github.com>
2021-08-21 01:05:07 +0700

    Update admin.js

--- modules/commands/admin.js
+++ modules/commands/admin.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "admin",
 	version: "1.0.5",
 	hasPermssion: 0,
-	credits: "RAIDEN",
+	credits: "Raiden",
 	description: "Quản lý admin bot.",
 	commandCategory: "config",
 	usages: "[list/add/remove] [userID]",
