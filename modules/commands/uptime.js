e0a7c89b08bd8a6574f49aae58e0a2b941ff8b59
Minh Khiêm <87948587+taobotday@users.noreply.github.com>
2021-08-21 10:25:08 +0700

    Update uptime.js

--- modules/commands/uptime.js
+++ modules/commands/uptime.js
@@ -1,36 +1,31 @@
 module.exports.config = {
-	name:"uptime",
-	version: "1.0.0",
-	hasPermssion: 0,
-	credits: "HelyT",
-	description: "Random ảnh theo api - uptime",
+	name: "uptime",
+	version: "1.0.1",
+	hasPermssion: 2,
+	credits: "TBĐ",
+	description: "Thời gian bot đã hoạt động.",
 	commandCategory: "system",
-	cooldowns: 3
+	cooldowns: 5,
+	dependencies: {
+		"pidusage": ""
+	}
 };
+
 function byte2mb(bytes) {
 	const units = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
 	let l = 0, n = parseInt(bytes, 10) || 0;
 	while (n >= 1024 && ++l) n = n / 1024;
 	return `${n.toFixed(n < 10 && l > 0 ? 1 : 0)} ${units[l]}`;
 }
+
 module.exports.run = async ({ api, event }) => {
-	const axios = require('axios');
-	const request = require('request');
-	const fs = require("fs");
-const time = process.uptime(),
+	const time = process.uptime(),
 		hours = Math.floor(time / (60 * 60)),
 		minutes = Math.floor((time % (60 * 60)) / 60),
 		seconds = Math.floor(time % 60);
+
 	const pidusage = await global.nodemodule["pidusage"](process.pid);
+
 	const timeStart = Date.now();
-	axios.get('https://api.vangbanlanhat.tk/image?type=boy').then(res => {
-	let ext = res.data.data.substring(res.data.data.lastIndexOf(".") + 1);
-	let callback = function () {
-					api.sendMessage({
-                                                body: `Bot của HelyT (Trường Giang BOT) đã hoạt động được ${hours} giờ ${minutes} phút ${seconds} giây ❤️🖕.\n\n❯ Tổng người dùng: ${global.data.allUserID.length}\n❯ Tổng nhóm: ${global.data.allThreadID.length}\n❯ Cpu đang sử dụng: ${pidusage.cpu.toFixed(1)}\n❯ Ram đang sử dụng: ${byte2mb(pidusage.memory)}\n❯ Ping: ${Date.now() - timeStart}`,
-						attachment: fs.createReadStream(__dirname + `/cache/anh.${ext}`)
-					}, event.threadID, () => fs.unlinkSync(__dirname + `/cache/anh.${ext}`), event.messageID);
-				};
-				request(res.data.data).pipe(fs.createWriteStream(__dirname + `/cache/anh.${ext}`)).on("close", callback);
-			})
+	return api.sendMessage("", event.threadID, () => api.sendMessage(`Bot đã hoạt động được ${hours} giờ ${minutes} phút ${seconds} giây.\n\n➤ Tổng người dùng: ${global.data.allUserID.length}\n➤ Tổng Nhóm: ${global.data.allThreadID.length}\n➤ Cpu đang sử dụng: ${pidusage.cpu.toFixed(1)}%\n➤ Ram đang sử dụng: ${byte2mb(pidusage.memory)}\n➤ Ping: ${Date.now() - timeStart}ms`, event.threadID, event.messageID));
 }
