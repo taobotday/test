37eaa4f4ca04320dd8d0001ba47eca40d5ea0fb5
Minh Khiêm <87948587+taobotday@users.noreply.github.com>
2021-08-21 09:53:08 +0700

    Update ngu.js

--- modules/commands/ngu.js
+++ modules/commands/ngu.js
@@ -3,17 +3,17 @@ module.exports.config = {
 name: "ngu",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "VanHung",
-	description: "Ngu như m =)",
-	commandCategory: "Không cần dấu lệnh",
-	usages: "ko cần prefix chỉ cần chat ngu",
+	credits: "Raiden",
+	description: "Ngu như m =)))",
+	commandCategory: "",
+	usages: "",
 	cooldowns: 5,
 };
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("ngu")==0 || (event.body.indexOf("Ngu")==0)) {
 		var msg = {
-				body: "Trường hợp này bó tay !",
+				body: "Trường hợp này bó tay!",
 				attachment: fs.createReadStream(__dirname + `/noprefix/ngu.mp3`)
 			}
 			api.sendMessage(msg, threadID, messageID);
