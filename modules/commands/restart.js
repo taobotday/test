182c65c9f5ffe1a046e13621c45d4a1654187f91
Minh Khiêm <87948587+taobotday@users.noreply.github.com>
2021-08-21 10:29:22 +0700

    Update restart.js

--- modules/commands/restart.js
+++ modules/commands/restart.js
@@ -2,9 +2,9 @@ module.exports.config = {
 	name: "restart",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "Mirai Team",
+	credits: "Raiden",
 	description: "Khởi Động Lại Bot.",
 	commandCategory: "system",
 	cooldowns: 0
         };
-module.exports.run = ({event, api}) =>api.sendMessage("Địt con mẹ chờ bố mày 1p khởi động lại ... ",event.threadID, () =>process.exit(1))
+module.exports.run = ({event, api}) =>api.sendMessage("Chờ tao 1 phút khởi động lại...",event.threadID, () =>process.exit(1))
